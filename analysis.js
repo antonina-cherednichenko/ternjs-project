5475c0438ae840bb19f1a1c230358502e9c50528
tonya <antonina.cherednichenko@gmail.com>
2016-03-25 18:33:16 +0200

    Add defs/refs

--- analysis.js
+++ analysis.js
@@ -108,27 +108,23 @@ function analyse_all() {
             if (node.id) {
                 c(node.id, st);
 
-                console.error("Function id = ", node.id);
+                //console.error("Function id = ", node.id);
                 //emit fn def here
-                // var fnDef = {
-                //     Path: form_path(node.id),
-                //     Name: node.id.name,
-                //     Kind: "fn",
-                //     File: decl.sourceFile.name,
-                //     DefStart: decl.id.start,
-                //     DefEnd: decl.id.end,
-                //     TreePath: form_path(decl.id)
-                // }
-                //out.Defs.push(fnDef);
-
+                var fnDef = {
+                    Path: form_path(node.id),
+                    Name: node.id.name,
+                    Kind: "fn",
+                    File: decl.sourceFile.name,
+                    DefStart: decl.id.start,
+                    DefEnd: decl.id.end,
+                    TreePath: form_path(decl.id)
+                }
+                out.Defs.push(fnDef);
             }
 
             for (var i = 0; i < node.params.length; ++i)
                 c(node.params[i], st);
             c(node.body, st);
-        },
-        ExpressionStatement: function(node, st, c) {
-            console.error(node);
         },
 
         // TryStatement: function(node, st, c) {
@@ -158,57 +154,8 @@ function analyse_all() {
                 if (decl.init) c(decl.init, st);
             }
         },
-        Identifier: function(node, file, c) {
-
-            console.error("Here inside identifier = ", node.name);
-            // var wordStart = node.start;
-            // var exprAt = infer.findExpressionAround(file.ast, null, wordStart, file.scope);
-            // console.error("Id = ", node.name);
-            // try {
-            //     //console.error("DEFINITON = ", defnode.findOriginPseudonode(file.ast, node.start, node.end));
-            //     console.error("DEFINITON = ", defnode.findNameNodes(file.ast, node.start, node.end));
-            // } catch (e) {
-            //     console.error(e);
-            // }
-            //console.error("DEFINITON = ", defnode.findDefinitionNode(file.ast, node.start, node.end));
-            // console.error("expr = ", exprAt);
-            // var memberExpr, objLit;
-            // Decide whether this is an object property, either in a member
-            // expression or an object literal.
-            // if (exprAt) {
-            //     var exprNode = exprAt.node;
-            //     if (exprNode.type == "MemberExpression" && exprNode.object.end < wordStart) {
-            //         memberExpr = exprAt;
-            //     }
-            //     if (memberExpr) {
-            //         prop = memberExpr.node.property;
-            //         prop = prop.type == "Literal" ? prop.value.slice(1) : prop.name;
-            //         memberExpr.node = memberExpr.node.object;
-            //         objType = infer.expressionType(memberExpr);
-            //         console.error("property = ", prop);
-            //         console.error("TYPE = ", objType);
-            //     }
-            //console.error(node);
-            //console.error("ID = ", node.name, "FILE = ", node.sourceFile.name);
-            //getDefinition(file.name, node.end, node.start);
-            //getCompletions(file.name, node.end, node.start);
-            //getType(st, node.end, node.start);
-            //getCompletions(st, node.end, node.start);
-            //console.error(st);
-            // var expr;
-            // try {
-            //     expr = tern.findQueryExpr(st, ident);
-            // } catch (e) {
-            //     console.error('No expression at ' + st.name + ':' + node.start + '-' + node.end);
-            //     return null;
-            // }
-            //console.error("Query expr = ", tern.findQueryExpr(st, node));
-
-        }
-    });
-
-    var fullVisitor = walk.make({
         MemberExpression: function(node, st, c) {
+           // console.error('member expr =', node);
             c(node.object, st, "Expression");
             c(node.property, st);
         },
@@ -217,35 +164,26 @@ function analyse_all() {
                 c(node.properties[i].value, st);
                 c(node.properties[i].key, st);
             }
+        },
+        Identifier: function(node, file, c) {
+            //console.error("Here inside identifier = ", node);
+            var ref = {
+                DefPath: form_path(node),
+                Def: false,
+                File: node.sourceFile.name,
+                Start: node.start,
+                End: node.end
+            }
+            out.Refs.push(ref);
         }
-    }, searchVisitor);
-
-
-
-
-    // var visitor = {
-    //     FunctionDeclaration: function(node) {
-    //         console.error("FN DECL");
-    //         console.error(node);
-    //         walk.simple(node.body, visitor);
-    //         //walk.simple(node.params, visitor);
-
-    //     },
-
-    //     Identifier: function(node) {
-    //         console.error("Here inside identifier = ", node);
-    //     }
-    // };
-
-    ternServer.files.forEach(function(file) {
-        //var code = fs.readFileSync(file, 'utf8');
-        //var ast = acorn.parse(code);
-        //console.error("AST = ", ast);
-        //console.error(file.ast);
-        walk.recursive(file.ast, file, null, fullVisitor);
-        //walk.simple(ast, visitor);
-
     });
 
-    //console.error("DEFS = ", out.Defs);
+
+    ternServer.files.forEach(function(file) {
+        //console.error(file.ast);
+        walk.recursive(file.ast, file, null, searchVisitor);
+    });
+
+    console.error("DEFS = ", out.Defs);
+    console.error("REFS = ", out.Refs);
 }
